3a2e8a865398f5d37716a53a5a074b9bb127c628
BinaryBecca <rebecca.thinnes@web.de>
2025-08-12 14:50:42 +0200

    typescript

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,32 @@
 import "./style.css"
+
+const userBodySizeInput = document.getElementById("size") as HTMLInputElement
+const userAgeInput = document.getElementById("age") as HTMLInputElement
+
+const smallAnatomy = document.querySelector<HTMLInputElement>('input[value="small"]')
+const bigAnatomy = document.querySelector<HTMLInputElement>('input[value="big"]')
+
+const calculatorButton = document.getElementById("submit") as HTMLButtonElement
+const result = document.getElementById("result") as HTMLDivElement
+
+calculatorButton.addEventListener("submit", function (e) {
+  e.preventDefault()
+
+  const userBodySizeValue = Number(userBodySizeInput.value)
+  const userAgeValue = Number(userAgeInput.value)
+
+  const userSmall = smallAnatomy?.checked
+  const userBig = bigAnatomy?.checked
+
+  if (userSmall) {
+    const userSmallWeight = userBodySizeValue - 100 + (userAgeValue / 10) * 0.9 * 0.9
+    result.innerText = `Dein Idealgewicht ist ${userSmallWeight}`
+    // console.log(userSmallWeight)
+  } else if (userBig) {
+    const userBigWeight = userBodySizeValue - 100 + (userAgeValue / 10) * 0.9 * 1.1
+    result.innerText = `Dein Idealgewicht ist ${userBigWeight}`
+  }
+
+  // console.log(userSmall)
+  // console.log(userBig)
+})
